3b6d5c5eb1db3a2adb96068a298106e2c5da4392
Jenny Pletner <jennykay55@gmail.com>
2015-10-20 16:49:57 -0700

    Final Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Learn Academy Exercise: Traversing with JQuery Using 'this'
+
+    *Created an application that provides information on popular vacation locations
+    *User can add to list of places they'd like to visit on click of a button
+    *User can remove a place from the list by selecting the item
+
+To Run: Launch index.html file from local browser
--- jquery.js
+++ jquery.js
@@ -1,21 +1,21 @@
 
 $(document).ready(function() {
 
+//on toggle click of image, details of place are displayed/removed    
   $('img').click(function() {
     $(this).parent('div').toggleClass('state2');
   });
 
+ //add name of place to sidebar list when 'Add to Trips' button is selected   
   $("button").click(function() {
     var head = $(this).parent().children('h1').text();
-
-      $('ul#tripList').prepend('<li>' + head + '</li>');
+      $('ul#tripList').prepend(" " + '<li>' + head + " |  "+ '</li>');
       $("ul#tripList").children("li").click(function() {
-          $(this).remove();
+          $(this).remove(); //on click of list item, remove from list
       });
   });
 
-
-
+//animated footer (for fun)
   $(".footer").click(function(){
     var div = $(".footer");
         div.animate({height: '300px', opacity: '0.4'}, "slow");
